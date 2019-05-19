2308a46ca75b4a6f797128086abfe2e4202caaa3
Alexander Travov <alexander.travov@gmail.com>
2019-05-19 14:52:10 +0300

    Add helper plotter for classifier decision regions

---
+++ utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    """
+    X = R2.
+    Draw colored decision regions for classifier on a plane.
+    """
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min()-1, X[:, 0].max()+1
+    x2_min, x2_max = X[:, 1].min()-1, X[:, 1].max()+1
+    xx1, xx2 = np.meshgrid(
+        np.arange(x1_min, x1_max, resolution),
+        np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]))
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+
+    for idx, cl in enumerate(np.unique(y)):
+        class_mask = y==cl
+        plt.scatter(x=X[class_mask, 0], y=X[class_mask, 1],
+            alpha=0.8,
+            c=colors[idx],
+            marker=markers[idx],
+            label=cl,
+            edgecolor='black')
